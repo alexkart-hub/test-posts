373aa806ab4610dea7ed657295279ddd31b5b36d
Alexander Kartavtsev <ugo-lok@yandex.ru>
2023-08-02 08:59:35 +0300

    TP. Добавил страницу для отображения постов

--- .top.menu.php
+++ .top.menu.php
@@ -41,6 +41,13 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
+	),
+	Array(
+		"Посты", 
+		"/posts.php", 
+		Array(), 
+		Array(), 
+		"" 
 	)
 );
 ?>
---
+++ posts.php
@@ -0,0 +1,5 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Посты");
+$APPLICATION->SetTitle("Посты");
+?>Здесь будут посты<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
